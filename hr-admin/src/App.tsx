84c53867c430aaa1a89e9755dee664ec81bc9d69
Mojtaba Hassani <123.hasani@gmail.com>
2025-06-27 23:14:32 +0330

    auto: 2025-06-27 23:14

--- hr-admin/src/App.tsx
+++ hr-admin/src/App.tsx
@@ -3,56 +3,43 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
 import Layout from './components/layout/Layout';
-
-// Import صفحات
 import Dashboard from './pages/Dashboard';
 import ComingSoon from './pages/ComingSoon';
 
-// کامپوننت اصلی App که شامل ThemeProvider است
-const AppContent: React.FC = () => {
-  return (
-    <Router 
-      future={{
-        v7_startTransition: true,
-        v7_relativeSplatPath: true
-      }}
-    >
-      <Layout>
-        <Routes>
-          {/* صفحه اصلی - داشبورد */}
-          <Route path="/" element={<Dashboard />} />
-          
-          {/* صفحات مختلف سیستم */}
-          <Route path="/sales" element={<ComingSoon title="مدیریت فروش" />} />
-          <Route path="/customers" element={<ComingSoon title="مدیریت مشتریان" />} />
-          <Route path="/employees" element={<ComingSoon title="مدیریت پرسنل" />} />
-          <Route path="/investors" element={<ComingSoon title="سرمایه گزاران" />} />
-          <Route path="/users" element={<ComingSoon title="مدیریت کاربران" />} />
-          <Route path="/tags" element={<ComingSoon title="مدیریت برچسب ها" />} />
-          <Route path="/forms" element={<ComingSoon title="مدیریت فرم ها" />} />
-          <Route path="/settings" element={<ComingSoon title="تنظیمات" />} />
-          <Route path="/sms" element={<ComingSoon title="مدیریت پیامک" />} />
-          <Route path="/documents" element={<ComingSoon title="مدیریت اسناد" />} />
-          <Route path="/ai" element={<ComingSoon title="هوش مصنوعی" />} />
-          <Route path="/reports" element={<ComingSoon title="گزارشات" />} />
-          <Route path="/workflows" element={<ComingSoon title="مدیریت فرایندها" />} />
-          <Route path="/databases" element={<ComingSoon title="مدیریت پایگاه داده ها" />} />
-          
-          {/* صفحه 404 */}
-          <Route path="*" element={<ComingSoon title="صفحه یافت نشد" />} />
-        </Routes>
-      </Layout>
-    </Router>
-  );
-};
+// Firebase Connection Test (فقط در development)
+import FirebaseConnectionTest from './components/common/FirebaseConnectionTest';
 
-// کامپوننت اصلی App
-const App: React.FC = () => {
+function App() {
   return (
     <ThemeProvider>
-      <AppContent />
+      <Router>
+        <div className="App min-h-screen bg-gray-50 dark:bg-gray-900">
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              
+              {/* صفحات فرم‌ساز - فعلاً Coming Soon */}
+              <Route path="/forms" element={<ComingSoon />} />
+              <Route path="/forms/create" element={<ComingSoon />} />
+              <Route path="/forms/:id/edit" element={<ComingSoon />} />
+              <Route path="/forms/:id/data" element={<ComingSoon />} />
+              
+              {/* سایر صفحات */}
+              <Route path="/employees" element={<ComingSoon />} />
+              <Route path="/payroll" element={<ComingSoon />} />
+              <Route path="/attendance" element={<ComingSoon />} />
+              <Route path="/reports" element={<ComingSoon />} />
+              <Route path="/settings" element={<ComingSoon />} />
+            </Routes>
+          </Layout>
+          
+          {/* Firebase Connection Test - فقط در development */}
+          <FirebaseConnectionTest />
+        </div>
+      </Router>
     </ThemeProvider>
   );
-};
+}
 
 export default App;
