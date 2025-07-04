bf42f1fba7f1756209ccc8e6f0818467d00178a5
Mojtaba Hassani <noreply@github.com>
2025-07-04 09:54:27 +0330

    auto: 2025-07-04 09:54

--- hr-admin/src/modules/form-builder/types/index.ts
+++ hr-admin/src/modules/form-builder/types/index.ts
@@ -9,25 +9,7 @@ export * from './form.types';
 export * from './field.types';
 
 // Database types - excluding conflicting ones
-export type {
-  DatabaseType,
-  DatabaseConfig,
-  ApiResponse,
-  PaginatedResponse,
-  FormFilters,
-  PaginationOptions,
-  PaginatedResult,
-  SortOptions,
-  ExportOptions,
-  ImportOptions,
-  BatchResult,
-  DatabaseStats,
-  HealthCheckResult,
-  CreateFormDto,
-  UpdateFormDto,
-  ValidationErrorType,
-  ValidationResult
-} from './database.types';
+export * from './database.types';
 
 // Re-export FormResponse and FormTemplate from form.types to avoid conflicts
 export type { FormResponse, FormTemplate } from './form.types';
