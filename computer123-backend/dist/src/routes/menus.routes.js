96071acb68356330984db33498325743c912dbb3
Mojtaba Hassani <noreply@github.com>
2025-07-05 06:57:51 +0330

    auto: 2025-07-05 06:57

--- computer123-backend/dist/src/routes/menus.routes.js
+++ computer123-backend/dist/src/routes/menus.routes.js
@@ -10,8 +10,6 @@ router.get('/:id', menuController.getMenu.bind(menuController));
 router.post('/', validate_middleware_1.validateMenu, menuController.createMenu.bind(menuController));
 router.put('/:id', validate_middleware_1.validateMenu, menuController.updateMenu.bind(menuController));
 router.delete('/:id', menuController.deleteMenu.bind(menuController));
-router.post('/reorder', menuController.reorderMenus.bind(menuController));
-router.post('/:id/move', menuController.moveMenu.bind(menuController));
 router.get('/', menuController.getMenus.bind(menuController));
 router.get('/:id/children', menuController.getMenuWithChildren.bind(menuController));
 exports.default = router;
