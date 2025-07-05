cf5b97473dcf301f3d804d9f6fa85583577fb318
Mojtaba Hassani <noreply@github.com>
2025-07-05 05:38:56 +0330

    auto: 2025-07-05 05:38

--- hr-admin/src/modules/form-builder/components/index.ts
+++ hr-admin/src/modules/form-builder/components/index.ts
@@ -9,6 +9,7 @@ export { default as FormBuilder } from './FormBuilder/FormBuilder';
 export { default as FieldsPanel } from './FormBuilder/FieldsPanel';
 export { default as PreviewPanel } from './FormBuilder/PreviewPanel';
 export { default as SettingsPanel } from './FormBuilder/SettingsPanel';
+export { default as SidePanel } from './FormBuilder/SidePanel';
 
 // Forms List Components
 export { FormsList, FormCard, CreateFormModal } from './FormsList';
